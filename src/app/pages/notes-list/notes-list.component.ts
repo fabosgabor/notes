13c283f3ccb8dfec33287c3752753851cdfa897c
fabosgabor <82319001+fabosgabor@users.noreply.github.com>
2021-04-12 21:45:59 +0200

    Add files via upload

---
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Note } from 'src/app/shared/note.model';
+import { NotesService } from 'src/app/shared/notes.service';
+
+@Component({
+  selector: 'app-notes-list',
+  templateUrl: './notes-list.component.html',
+  styleUrls: ['./notes-list.component.scss']
+})
+export class NotesListComponent implements OnInit {
+
+  notes: Note[] = new Array<Note>();
+
+  constructor(private notesService: NotesService) { }
+
+  ngOnInit() {
+    this.notes = this.notesService.getAll();
+
+  }
+
+  deleteNote(id: number){
+    this.notesService.delete(id);
+
+  }
+}
